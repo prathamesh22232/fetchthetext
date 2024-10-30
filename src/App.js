85c1f5c9ddd98d6c754f07af6f9299714223ef63
Atharva Kulkarni <atharvak706@gmail.com>
2024-10-30 11:19:56 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+//import TextForm from './components/TextForm'
+import Navbar from './components/Navbar';
+
+//import Alert from './components/Alert'
+//import About from './components/About'
 import './App.css';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+        <> 
+        <Navbar/>
+          <TextForm/>
+            
+        </>
   );
 }
 
